82e0614f1669f9a2340a51a73e5a70b4effca4d7
rakesh <rakesh@DESKTOP-C3Q21H4>
2021-02-26 15:03:48 +0530

    First commit

---
+++ src/main/java/com/app/GitHubSampleAppApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubSampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubSampleAppApplication.class, args);
+	}
+
+}
